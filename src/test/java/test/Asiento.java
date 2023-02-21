9aec51792623473fe3cd1e67c3937a82ab157279
snoopbrain <ancarvajalb@unal.edu.co>
2023-02-21 01:44:36 -0500

    dos

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,15 +1,8 @@
 package test;
 public class Asiento {
-    private String color;
-    private int precio;
-    private int registro;
-    
-    // Constructor
-    public Asiento(String color, int precio, int registro) {
-        this.color = color;
-        this.precio = precio;
-        this.registro = registro;
-    }
+    public String color;
+    public int precio;
+    public int registro;
     
     public void cambiarColor(String color) {
         if (color.equals("rojo") || color.equals("verde") || color.equals("amarillo") || color.equals("negro") || color.equals("blanco")) 

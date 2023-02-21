af868e5b77423bcb7d702a1b5f7c6145d74d0683
snoopbrain <ancarvajalb@unal.edu.co>
2023-02-21 02:29:49 -0500

    uwu

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,3 +1,34 @@
+package test;
 public class Auto {
-    
+    public String modelo;
+    public int precio;
+    public Asiento[] asientos;
+    public String marca;
+    public Motor motor;
+    public int registro;
+    public static int cantidadCreados;
+
+    public int getCantidadAsientos() {
+        int cantidad = 0;
+        for (Asiento asiento : asientos) {
+            if (asiento != null) {
+                cantidad++;
+            }
+        }
+        return cantidad;
+    }
+
+    public String verificarIntegridad() {
+        for (Asiento asiento : asientos){
+            if (asiento !=null){
+                if ((motor.registro != registro) || (asiento.registro != registro)){
+                    return "Las piezas no son originales";
+        }  
+            
+        }
+        }
+        return "Auto original";
+    }
 }
+
+
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,3 +1,16 @@
+package test;
 public class Motor {
+    public int registro;
+    public String tipo;
+    public int numeroCilindros;
     
+    public void cambiarRegistro(int registro) {
+    this.registro = registro;
+    }
+
+    public void asignarTipo(String tipo) {
+    if (tipo.equals("gasolina") || tipo.equals("electrico")) {
+        this.tipo = tipo;
+    }
+    }
 }
